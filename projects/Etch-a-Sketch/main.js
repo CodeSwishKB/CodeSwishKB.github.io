98e2668111cacbdc47edda0e8f434490dade612a
CodeSwishKB <kylebarco04@gmail.com>
2024-01-11 11:00:22 +0800

    make it responsive

--- projects/Etch-a-Sketch/main.js
+++ projects/Etch-a-Sketch/main.js
@@ -1,12 +1,35 @@
 const DEFAULT_SIZE = 16
-const GRIDSIDE = 600
+const DEFAULT_MODE = 'color'
+
+const GRIDWIDTH = 600
+const GRIDHEIGHT = 600
 
 let currentSize = DEFAULT_SIZE
+let currentMode = DEFAULT_MODE
 
 const sketch = document.querySelector('.sketch')
-const btn = document.querySelector("#btn")
+const generateBtn = document.querySelector(".change-btn")
+const randBtn = document.querySelector('.random-btn')
+const defaultBtn = document.querySelector('.default-btn')
 
-sketch.style.width = sketch.style.height = `${GRIDSIDE}px`
+sketch.style.width = `${GRIDWIDTH}px`
+sketch.style.height = `${GRIDHEIGHT}px`
+
+makeFlexRows(currentSize)
+
+function setCurrentMode(newMode){
+    currentMode = newMode
+}
+
+function addColor(e){
+    if (e.type === 'mouseover' && !mouseDown) return
+
+    if (currentMode === 'rainbow'){
+        e.target.style.backgroundColor = randomColor()
+    } else if (currentMode === 'color'){
+        e.target.style.backgroundColor = '#000'
+    }
+}
 
 function removeGrid(){
     while(sketch.firstChild){
@@ -16,20 +39,27 @@ function removeGrid(){
 
 function makeFlexRows(square){
     const numOfSquares = (square * square)
-    const widthNHeight = `${(GRIDSIDE / square) - 2}px`
+    const width = `${(GRIDWIDTH / square) - 2}px`
+    const height = `${(GRIDHEIGHT / square) - 2}px`
 
     for (i = 0; i < numOfSquares; i++ ){
         let cell = document.createElement('div')
          
-        cell.style.width = cell.style.height = widthNHeight
+        cell.style.width = width
+        cell.style.height = height
         cell.classList.add('flex-item')
         sketch.appendChild(cell)
-        
-        sketch.addEventListener('mouseover', addColor)
-        sketch.addEventListener('mousedown', addColor)
-    }
+
+                clickColor(addColor)
+         
+        }
+    
 }
 
+function clickColor(color){
+    sketch.addEventListener('mouseover', color)
+    sketch.addEventListener('mousedown', color)
+}
 
 let mouseDown = false
 document.body.onmousedown = () => {
@@ -39,18 +69,28 @@ document.body.onmouseup = () => {
     mouseDown = false
 }
 
-function addDivs(){
-    let input = prompt('Enter value of divs (max is 100).', DEFAULT_SIZE)
-    while (input >= 100) {
-        input = prompt('Max is 100')
+function generateDivs(){
+    let input = prompt('Enter value of divs (max is 64).', DEFAULT_SIZE)
+    while (input > 64) {
+        input = prompt('Max is 64')
     }
     removeGrid()
     makeFlexRows(input)
 }
 
-function addColor(e){
-    if (e.type === 'mouseover' && !mouseDown) return
-    e.target.classList.add('black')
+function randomColor(){
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    const rgb = `rgb(${r},${g},${b})`; // Collect all to a css color string
+    return rgb
 }
 
-btn.addEventListener('click', addDivs)
+// randBtn.addEventListener('click', setCurrentMode('rainbow'))
+// defaultBtn.addEventListener('click', setCurrentMode('color'))
+
+randBtn.onclick = () => setCurrentMode('rainbow')
+defaultBtn.onclick = () => setCurrentMode('color')
+
+generateBtn.addEventListener('click', generateDivs)
