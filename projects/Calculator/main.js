324242bdbcddab2f655e20aa73dc8a41b5a3a480
CodeSwishKB <kylebarco04@gmail.com>
2024-01-25 11:58:59 +0800

    change the operate function

--- projects/Calculator/main.js
+++ projects/Calculator/main.js
@@ -5,23 +5,28 @@ let op = ""
 let secondNum = ""
 
 const numsBtn = document.querySelectorAll('.numop') 
-const operatorsBtn = document.querySelectorAll('.operator') 
-const equalBtn = document.querySelector('.equal')
-const decBtn = document.querySelector('.dec')
-const clearBtn = document.querySelector('.clear')
 
+const operatorsBtn = document.querySelectorAll('.operator') 
+
+const equalBtn = document.querySelector('.equal')
+
+const decBtn = document.querySelector('.dec')
+
+const clearBtn = document.querySelector('.clear')
+clearBtn.addEventListener('click', clearNum)
 equalBtn.addEventListener("click", operate)
 
-numsBtn.forEach((numBtn) => {
-    numBtn.addEventListener('click', (e) => {
-        handlerNum(e.target.textContent)
+    numsBtn.forEach((numBtn) => {
+        numBtn.addEventListener('click', (e) => {
+            handlerNum(e.target.textContent)
+        })
     })
-})
+
 
 function handlerNum(num){
-    if(firstNum.length <= 11){
-        firstNum += num
-        elDisplay.textContent = firstNum
+    if(secondNum.length <= 11){
+        secondNum += num
+        elDisplay.textContent = secondNum
     }
 }
 
@@ -34,34 +39,61 @@ operatorsBtn.forEach(operatorBtn => {
 function handleOperator(ops){
     op = ops
     firstNum = secondNum
-    elDisplay.textContent = `${firstNum} ${op} `
+    secondNum = ""
+    elDisplay.textContent = `${firstNum} ${op} ${secondNum}`
 }
 
 function operate(){    
     firstNum = Number(firstNum)
     secondNum = Number(secondNum)
-  
+
         switch(op){
             case '+':
-                 firstNum += secondNum
+                addNum(firstNum, secondNum)
                 break;
             
             case '-':
-                firstNum -= secondNum
+                subtractNum(firstNum, secondNum)
                 break;
             
-            case '*':
-                firstNum *= secondNum
+            case 'x':
+                multiplyNum(firstNum, secondNum)
                 break;
     
-            case '/':
-                if (secondNum == 0){
-                    console.log('Can\'t divide by 0 ')  
-                }  else {
-                    firstNum /= secondNum
-                } 
+            case '÷':
+                if (secondNum <= 0){
+                    elDisplay.textContent = 'Can\'t divide by 0 '  
+                } else {
+                    divideNum(firstNum, secondNum)
+                    
+                }
+                break
         }
-        elDisplay.textContent = firstNum
-        console.log(firstNum)
-        console.log(secondNum)
+        
+}
+
+function addNum(num1, num2){
+     num1 += num2
+     elDisplay.textContent = num1
+}
+function subtractNum(num1, num2){
+     num1 -= num2
+     elDisplay.textContent = num1
+
+}
+function multiplyNum(num1, num2){
+    num1 *= num2
+    elDisplay.textContent = num1
+
+}
+function divideNum(num1, num2){
+     num1 /= num2
+     elDisplay.textContent = num1
+
+}
+
+function clearNum(){
+    firstNum = ""
+    secondNum = ""
+    elDisplay.textContent = 0
 }
