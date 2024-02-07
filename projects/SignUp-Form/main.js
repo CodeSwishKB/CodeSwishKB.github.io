9f52d9b9070bd657a11644131edcb5696c8f21f2
CodeSwishKB <kylebarco04@gmail.com>
2024-02-08 07:11:47 +0800

    finished form

--- projects/SignUp-Form/main.js
+++ projects/SignUp-Form/main.js
@@ -9,6 +9,17 @@ const confirmPwd = document.querySelector("#confirm_pwd");
 
 const inputs = [fname, lname, email, phone, pwd, confirmPwd];
 
+function showErrorPwd(password, confirmPassword, msg, msg2) {
+  const formControl = password.parentElement;
+  const formControl2 = confirmPassword.parentElement;
+  formControl.className = "form__info-item error";
+  formControl2.className = "form__info-item error";
+  const small = formControl.querySelector("small");
+  const small2 = formControl2.querySelector("small");
+  small.textContent = msg;
+  small2.textContent = msg2;
+}
+
 function showError(input, msg) {
   const formControl = input.parentElement;
   formControl.className = "form__info-item error";
@@ -23,16 +34,21 @@ function showSuccess(input) {
 
 function checkPasswordMatch(input1, input2) {
   if (input1.value !== input2.value) {
-    showError(input2, "* Password do not match");
+    showErrorPwd(
+      input1,
+      input2,
+      "* Please check it again",
+      "* Password do not match"
+      );
+    }
   }
-}
-
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-
-function checkFields(inputArr) {
-  inputArr.forEach((input) => {
+  
+  function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+  }
+  
+  function checkFields(inputArr) {
+    inputArr.forEach((input) => {
     if (input.value.trim() === "") {
       showError(input, `${getFieldName(input)} must put`);
     } else {
