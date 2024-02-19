4b6911aa3502ed9c06814b32b29cdf4095d57396
CodeSwishKB <kylebarco04@gmail.com>
2024-02-19 15:52:23 +0800

    so fuckin proud of this shit

--- odin-project/odin-project-exercises/js-section/Objects-and-Object-Constructors/Exercise/main.js
+++ odin-project/odin-project-exercises/js-section/Objects-and-Object-Constructors/Exercise/main.js
@@ -11,7 +11,7 @@ function Book(title, author, pages, read) {
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-// console.log(theHobbit.info());
+console.log(theHobbit.info());
 
 function Player(name, marker) {
   this.name = name;
@@ -30,7 +30,7 @@ const player2 = new Player("also steve", "O");
 // console.log(Object.getPrototypeOf(player2) === Player.prototype);
 // console.log(Object.getPrototypeOf(player2) === player1.prototype);
 
-console.log(player1.valueOf());
-console.log(player1.hasOwnProperty("valueOf")); // false
-console.log(Object.prototype.hasOwnProperty("valueOf"));
-console.log(Object.prototype.hasOwnProperty("hasOwnProperty"));
+// console.log(player1.valueOf());
+// console.log(player1.hasOwnProperty("valueOf")); // false
+// console.log(Object.prototype.hasOwnProperty("valueOf"));
+// console.log(Object.prototype.hasOwnProperty("hasOwnProperty"));
--- odin-project/projects/Book-Library/main.js
+++ odin-project/projects/Book-Library/main.js
@@ -1,9 +1,101 @@
-const myLibrary = []
+const addItemsBtn = document.querySelector("#addBtn");
+const modal = document.querySelector(".modal");
+const closeBtn = document.querySelector(".close");
+const addBook = document.querySelector('input[type="submit"]');
 
-function Book(){
+const tbody = document.querySelector("tbody");
+const author = document.querySelector("#author");
+const title = document.querySelector("#title");
+const numberOfPage = document.querySelector("#numOfPage");
+const readOrNot = document.querySelector("#readOrNot");
+let tableRow = 0;
 
+const authorInput = document.querySelector("#authorInput");
+const titleInput = document.querySelector("#titleInput");
+const pagesInput = document.querySelector("#pagesInput");
+const readOrNotInput = document.querySelector('input[name="read"]');
+const bookForm = document.querySelector(".book-form");
+
+const myLibrary = [];
+
+addItemsBtn.addEventListener("click", () => {
+  modal.style.visibility = "visible";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.visibility = "hidden";
+});
+
+function Book(author, title, numberOfPages, readOrNot) {
+  this.author = author;
+  this.title = title;
+  this.numberOfPages = numberOfPages;
+  this.readOrNot = readOrNot;
 }
 
-function addToLibrary(){
-    
+function addToLibrary() {
+  let newBook = new Book(
+    authorInput.value,
+    titleInput.value,
+    pagesInput.value,
+    readOrNotInput.value
+  );
+
+  myLibrary.push(newBook);
+  displayDetails(newBook);
+
+  bookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    e.target.reset();
+  });
+
+  modal.style.visibility = "hidden";
 }
+
+addBook.addEventListener("click", addToLibrary);
+
+myLibrary.forEach((item) => {
+  var x = item;
+  console.log(x);
+});
+
+console.log(myLibrary);
+
+// function showBooks() {
+//   let template = `
+//     <tr>
+//       <td>${this.author}</td>
+//       <td>${this.title}</td>
+//       <td>${this.pagesInput}</td>
+//       <td>${this.readOrNotInput}</td>
+//   `;
+//   myLibrary.forEach((book) => {
+//     book.innerHTML += template;
+//   });
+// }
+
+function displayDetails(newBook) {
+  // myLibrary.forEach(book1 => {
+  // })
+  let author = newBook.author;
+  let title = newBook.title;
+  let numberOfPage = newBook.numberOfPages;
+  let readOrNot = newBook.readOrNot;
+  let newRow = tbody.insertRow(tableRow);
+  let authorRow = newRow.insertCell(0);
+  let titleRow = newRow.insertCell(1);
+  let numberOfPageRow = newRow.insertCell(2);
+  let readOrNotRow = newRow.insertCell(3);
+  authorRow.textContent = author;
+  titleRow.textContent = title;
+  numberOfPageRow.textContent = numberOfPage;
+  readOrNotRow.textContent = readOrNot;
+  tableRow++;
+}
+
+// addToLibrary(book1);
+// addToLibrary(book2);
+
+// for (let key in book1) {
+//   console.log(`${key}: ${book1[key]}`);
+// }
