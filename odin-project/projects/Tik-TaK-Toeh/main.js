7bc0ea5af0be0db2f58085064e95119f00a53102
CodeSwishKB <kylebarco04@gmail.com>
2024-02-26 20:24:23 +0800

    finally i figure it out! tom is the ai thing

--- odin-project/projects/Tik-TaK-Toeh/main.js
+++ odin-project/projects/Tik-TaK-Toeh/main.js
@@ -3,50 +3,41 @@ function gameBoard() {
 	const columns = 3;
 	const board = [];
 
+	playerOneName = "Player One"
+	playerTwoName = "Player Two"
+
+	const score = 0
+
+	const players = [{
+			name: playerOneName,
+			value: "X",
+			score: score
+
+		},
+		{
+			name: playerTwoName,
+			value: "O",
+			score: score
+		}
+	]
+
+
 	for (let i = 0; i < rows; i++) {
 		board[i] = [];
 		for (let j = 0; j < columns; j++) {
-			// board[i][j] = 0
+			board[i][j] = 0
 		}
-        board[0][0] = 1
 	}
-	console.log(board)
 
+	const setVal = (row, col) => {
+		board[row][col] = players[0].value
+        console.log(players[0].name)
+        console.log(board)
+	}
+
+	return {
+		setVal
+	}
 }
 
-gameBoard()
-
-function gameController() {
-    playerOneName = "Player One"
-    playerTwoName = "Player Two"
-
-    const score = 0
-    
-    const players = [
-        {
-            name: playerOneName,
-            value: "X",
-            score: score
-
-        },
-        {
-            name: playerTwoName,
-            value: "O",
-            score: score
-        }
-    ]
-
-    const getVal = (p) => {
-        if(p === "Player One"){
-            return val = players[0].value
-        } else if (p === "Player Two") {
-            return val = players[1].value
-        }
-}
-
-    return {getVal}
-  
-}
-
-const player1 = gameController()
-
+const player1 = gameBoard()
