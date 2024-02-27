8d59ea6149dc21d3b664ced526a62c2fae7e1d82
CodeSwishKB <kylebarco04@gmail.com>
2024-02-27 11:59:41 +0800

    the ai thing is hard but i can do it anyways

--- odin-project/projects/Tik-TaK-Toeh/main.js
+++ odin-project/projects/Tik-TaK-Toeh/main.js
@@ -1,43 +1,90 @@
-function gameBoard() {
-	const rows = 3;
-	const columns = 3;
-	const board = [];
+function game() {
+	// const rows = 3;
+	// const columns = 3;
+	const board = [
+		["", "", ""], // 0 1 2
+		["", "", ""], // 3 4 5
+		["", "", ""]  // 6 7 8 
+	];
 
-	playerOneName = "Player One"
-	playerTwoName = "Player Two"
-
-	const score = 0
+	const winCombos = [
+		[0, 1, 2],
+		[3, 4, 5],	
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	]
+	
+		// for (let i = 0; i < rows; i++) {
+		// 	board[i] = [];
+		// 	for (let j = 0; j < columns; j++) {
+			// 		board[i][j] = "0"
+			// 	}
+			
+			// }
+			
+			const score = 0
 
 	const players = [{
-			name: playerOneName,
-			value: "X",
+			name: "Human",
+			option: "X",
 			score: score
-
 		},
 		{
-			name: playerTwoName,
-			value: "O",
+			name: "Ai",
+			option: "O",
 			score: score
 		}
 	]
 
+	// ESPECIALLY THIS PART 
+	let activePlayer = players[0];
 
-	for (let i = 0; i < rows; i++) {
-		board[i] = [];
-		for (let j = 0; j < columns; j++) {
-			board[i][j] = 0
+	const switchPlayerTurn = () => {
+		if (activePlayer === players[0]) {
+			activePlayer = players[1]
+		} else {
+			activePlayer = players[0]
 		}
 	}
 
-	const setVal = (row, col) => {
-		board[row][col] = players[0].value
-        console.log(players[0].name)
-        console.log(board)
+	// const switchPlayerTurn = () => {
+	// 	activePlayer = activePlayer === players[0] ? players[1] : players[0];
+	//   };
+
+	const getActivePlayer = () => activePlayer
+
+	const dropValue = (row, col) => {
+		if (activePlayer == players[0]) {
+			board[row][col] = players[0].option
+			console.log(players[0].name)
+			console.log(board)
+			switchPlayerTurn()
+			// activePlayer = players[1]
+		} else {
+			board[row][col] = players[1].option
+			console.log(players[1].name)
+			console.log(board)
+			switchPlayerTurn()
+			// activePlayer = players[0]
+
+		}
 	}
+
+	function emptyIndexies(board){
+		return  board.filter(s => s != "O" && s != "X");
+	  }
+
+	console.log(board)
 
 	return {
-		setVal
+		dropValue
 	}
+	// const getBoard = () => board
+
 }
 
-const player1 = gameBoard()
+const player = game()
