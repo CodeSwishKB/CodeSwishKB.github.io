fa9b92568e414a726b154f99b8be025fbdab97f8
CodeSwishKB <kylebarco04@gmail.com>
2024-03-11 09:25:06 +0800

    tab switching logic

--- odin-project/projects/Restaurant-Page/src/index.js
+++ odin-project/projects/Restaurant-Page/src/index.js
@@ -4,12 +4,13 @@ import logoMama from './components/img/logo.png'
 
 import { homePage } from './jsModules/home';
 import { menu } from './jsModules/menu';
+import { contact } from './jsModules/contact';
+
+// const homeBtn = document.querySelector('#home')
+// const menuBtn = document.querySelector('#menu')
+// const contactBtn = document.querySelector('#contact')
 
 const resto = (() => {
-    const homeBtn = document.querySelector('#home')
-    const menuBtn = document.querySelector('#menu')
-    const contactBtn = document.querySelector('#contact')
-
     const logo = () => {
         const header = document.querySelector('header')
         const imgLogo = document.createElement('img')
@@ -18,10 +19,21 @@ const resto = (() => {
         header.appendChild(imgLogo)
     }
 
+    const setUpTabs = () =>{
+        document.querySelectorAll('.tab-btn').forEach(button => {
+            button.addEventListener('click', () => {
+
+            })
+        })
+    }
+
 return {logo}
 
 })() 
 
+// homeBtn.addEventListener('click', homePage)
+// menuBtn.addEventListener('click', menu)
+// contactBtn.addEventListener('click', contact)
+
 resto.logo()
-// homePage()
-// menu()
+
--- odin-project/projects/Restaurant-Page/src/jsModules/contact.js
+++ odin-project/projects/Restaurant-Page/src/jsModules/contact.js
@@ -1,3 +1,32 @@
 export function contact(){
+    const divContent = document.querySelector('#content')
+    const elContainer = document.createElement('div')
+    const elH1 = document.createElement('h1')
+    const elH2 = document.createElement('h2')
+    const elP = document.createElement('p')
+    const socmedWrap = document.createElement('div')
+    const socmedList = document.createElement('ul')
+    let socmedIconItems = 
+        `<li class="list-items"><a href="#" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
+         <li class="list-items"><a href="#" target="_blank"><i class="fa-brands fa-x-twitter"></i></a></li>
+         <li class="list-items"><a href="#" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>`
+   
+    elH1.textContent = "Contact Us"
+    elH2.textContent = '350-03843-6969-5309'
+    elP.textContent = 'Mon - Fri 10:00AM - 6:00PM'
+    socmedList.innerHTML += socmedIconItems
+
+    elContainer.className = `contact-container`
+    socmedList.className = 'contact-ul'
+    elH1.className = 'mbn'
+    elH2.className = 'mbn'
+    elP.className = 'mbn'
+
+    divContent.appendChild(elContainer)
+    elContainer.appendChild(elH1)
+    elContainer.appendChild(elH2)
+    elContainer.appendChild(elP)
+    elContainer.appendChild(socmedWrap)
+    socmedWrap.appendChild(socmedList)
     
 }
