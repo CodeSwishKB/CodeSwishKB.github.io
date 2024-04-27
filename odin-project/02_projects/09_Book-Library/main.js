625d673838e49fff61f70da90c5d4e1d574b04f8
CodeSwish <kylebarco04@gmail.com>
2024-04-27 10:56:44 +0800

    branch for constraint validation

--- odin-project/02_projects/09_Book-Library/main.js
+++ odin-project/02_projects/09_Book-Library/main.js
@@ -1,61 +1,61 @@
 const addItemsBtn = document.querySelector("#addBtn");
 const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector(".close");
-const addBook = document.querySelector('.addBookBtn');
+const addBook = document.querySelector(".addBookBtn");
 
 const tbody = document.querySelector("tbody");
 const bookForm = document.querySelector(".book-form");
 
 addItemsBtn.addEventListener("click", () => {
-	modal.style.display = "block";
+  modal.style.display = "block";
 });
 
 closeBtn.addEventListener("click", () => {
-	modal.style.display = "none";
+  modal.style.display = "none";
 });
 
 class Book {
-	constructor(author, title, numberOfPages, read) {
-		this.author = author;
-		this.title = title;
-		this.numberOfPages = numberOfPages
-		this.read = read
-	}
+  constructor(author, title, numberOfPages, read) {
+    this.author = author;
+    this.title = title;
+    this.numberOfPages = numberOfPages;
+    this.read = read;
+  }
 
-	toggleRead() {
-		this.read = !this.read
-	}
+  toggleRead() {
+    this.read = !this.read;
+  }
 }
 
 function toggleRead(item) {
-	library.myLibrary[item].toggleRead()
-	library.showBooks()
+  library.myLibrary[item].toggleRead();
+  library.showBooks();
 }
 
 function removeBook(index) {
-	library.myLibrary.splice(index, 1)
-	library.showBooks()
+  library.myLibrary.splice(index, 1);
+  library.showBooks();
 }
 
 class Library {
-	constructor() {
-		this.myLibrary = []
-	}
+  constructor() {
+    this.myLibrary = [];
+  }
 
-	pushNewBook(newBook) {
-		this.myLibrary.push(newBook)
-	}
+  pushNewBook(newBook) {
+    this.myLibrary.push(newBook);
+  }
 
-	clearForm(author, title, pages) {
-		author.value = ""
-		title.value = ""
-		pages.value = ""
-	}
+  clearForm(author, title, pages) {
+    author.value = "";
+    title.value = "";
+    pages.value = "";
+  }
 
-	showBooks() {
-		tbody.innerHTML = '';
-		for (let i = 0; i < this.myLibrary.length; i++) {
-			let template = `
+  showBooks() {
+    tbody.innerHTML = "";
+    for (let i = 0; i < this.myLibrary.length; i++) {
+      let template = `
 		<tr>
 		  <td><p>${this.myLibrary[i].author}</p></td>
 		  <td><p>${this.myLibrary[i].title}</p></td>
@@ -71,66 +71,83 @@ class Library {
 		  </td>
 		</tr>
 		`;
-			tbody.innerHTML += template;
-		}
-	}
+      tbody.innerHTML += template;
+    }
+  }
 
-	showError(input, msg) {
-		const formControl = input.parentElement;
-		formControl.className = "form-info error";
-		const small = formControl.querySelector("small");
-		small.textContent = ""
-		small.textContent = msg;
-	}
+  // showError(input, msg) {
+  // 	const formControl = input.parentElement;
+  // 	formControl.className = "form-info error";
+  // 	const small = formControl.querySelector("small");
+  // 	small.textContent = ""
+  // 	small.textContent = msg;
+  // }
+  //
+  // showSuccess(input) {
+  // 	const formControl = input.parentElement;
+  // 	formControl.className = "form-info success";
+  // }
 
-	showSuccess(input) {
-		const formControl = input.parentElement;
-		formControl.className = "form-info success";
-	}
-
-	getFieldName(input) {
-		return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-	}
+  // getFieldName(input) {
+  // 	return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+  // }
 }
 
-let library = new Library()
+let library = new Library();
 
 bookForm.addEventListener("submit", (e) => {
-	e.preventDefault();
+  e.preventDefault();
 
-	const authorInput = document.querySelector("#author");
-	const titleInput = document.querySelector("#title");
-	const pagesInput = document.querySelector("#pages");
-	const read = document.querySelector('input[name="read"]');
+  const authorInput = document.querySelector("#author");
+  const titleInput = document.querySelector("#title");
+  const pagesInput = document.querySelector("#pages");
+  const read = document.querySelector('input[name="read"]');
 
-	let newBook = new Book(authorInput.value, titleInput.value, pagesInput.value, read.checked);
+  let newBook = new Book(
+    authorInput.value,
+    titleInput.value,
+    pagesInput.value,
+    read.checked,
+  );
 
-	if ((authorInput.value.trim() === "" || authorInput.value === null || authorInput.value === "") || (titleInput.value.trim() === "" || titleInput.value === null || titleInput.value === "") || (pagesInput.value.trim() === "" || pagesInput.value === null || pagesInput.value === "")) {
+  if (authorInput.validity.valueMissing) {
+    authorInput.setCustomValidity("Please enter a value");
+  } else if (titleInput.validity.valueMissing) {
+  } else {
+    library.pushNewBook(newBook);
+    library.showBooks(library);
+    modal.style.display = "none";
+    library.clearForm(authorInput, titleInput, pagesInput);
+  }
 
-		if (authorInput.value.trim() === "" || authorInput.value === null) {
-			library.showError(authorInput, `**${library.getFieldName(authorInput)} must put `);
-		} else {
-			library.showSuccess(authorInput);
-		}
+  // if ((authorInput.value.trim() === "" || authorInput.value === null || authorInput.value === "") || (titleInput.value.trim() === "" || titleInput.value === null || titleInput.value === "") || (pagesInput.value.trim() === "" || pagesInput.value === null || pagesInput.value === "")) {
+  //
+  // 	if (authorInput.value.trim() === "" || authorInput.value === null) {
+  // 		library.showError(authorInput, `**${library.getFieldName(authorInput)} must put `);
+  // 	} else {
+  // 		library.showSuccess(authorInput);
+  // 	}
+  //
+  // 	if (titleInput.value.trim() === "" || titleInput.value === null) {
+  // 		library.showError(titleInput, `**${library.getFieldName(titleInput)} must put `);
+  // 	} else {
+  // 		library.showSuccess(titleInput);
+  // 	}
+  //
+  // 	if (pagesInput.value.trim() === "" || pagesInput.value === null) {
+  // 		showError(pagesInput, `**${library.getFieldName(pagesInput)} must put `);
+  // 	} else {
+  // 		library.showSuccess(pagesInput);
+  // 	}
+  //
+  // } else {
+  // 	library.pushNewBook(newBook);
+  // 	library.showBooks(library);
+  // 	modal.style.display = "none";
+  // 	library.clearForm(authorInput, titleInput, pagesInput)
+  // 	console.log(library)
+  // }
+});
 
-		if (titleInput.value.trim() === "" || titleInput.value === null) {
-			library.showError(titleInput, `**${library.getFieldName(titleInput)} must put `);
-		} else {
-			library.showSuccess(titleInput);
-		}
+// Constraint validation
 
-		if (pagesInput.value.trim() === "" || pagesInput.value === null) {
-			showError(pagesInput, `**${library.getFieldName(pagesInput)} must put `);
-		} else {
-			library.showSuccess(pagesInput);
-		}
-
-	} else {
-		library.pushNewBook(newBook);
-		library.showBooks(library);
-		modal.style.display = "none";
-		library.clearForm(authorInput, titleInput, pagesInput)
-		console.log(library)
-	}
-
-})
