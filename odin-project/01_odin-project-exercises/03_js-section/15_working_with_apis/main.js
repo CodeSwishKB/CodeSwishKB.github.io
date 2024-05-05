a461c41e0ddae5f4d445f35aa4fc8eafd880b53d
CodeSwish <kylebarco04@gmail.com>
2024-05-05 10:06:44 +0800

    add: async/await

--- odin-project/01_odin-project-exercises/03_js-section/15_working_with_apis/main.js
+++ odin-project/01_odin-project-exercises/03_js-section/15_working_with_apis/main.js
@@ -13,17 +13,18 @@ searchForm.addEventListener("submit", (e) => {
 btn.addEventListener("click", init);
 
 function searchGif(query) {
-  let apiKey = "xA2CejGIc8wKgASK0Ybc4brkr0pOS98A";
-  fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}`, {
-    mode: "cors",
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((response) => {
-      console.log(response.data);
+  async function getGif() {
+    try {
+      let apiKey = "xA2CejGIc8wKgASK0Ybc4brkr0pOS98A";
+      const respose = await fetch(
+        `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}`,
+        {
+          mode: "cors",
+        },
+      );
+      const gifData = await respose.json();
       let result = "";
-      response.data.forEach((obj) => {
+      gifData.data.forEach((obj) => {
         const gifURL = obj.images.fixed_width.url;
         const height = obj.images.fixed_width.height;
         const width = obj.images.fixed_width.width;
@@ -31,22 +32,26 @@ function searchGif(query) {
         result += `<img class="imgClass" src="${gifURL} width="${width}" height="${height}" alt="${title}">`;
       });
       imgWrapper.innerHTML = result;
-    })
-    .catch((err) => displayError(err.message));
+    } catch (err) {
+      displayError(err);
+    }
+  }
+  getGif();
 }
 
 function defaultGif() {
-  let apiKey = "xA2CejGIc8wKgASK0Ybc4brkr0pOS98A";
-  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=kobe`, {
-    mode: "cors",
-  })
-    .then((response) => {
-      return response.json();
-    })
-    .then((response) => {
-      displayGif(response);
-    })
-    .catch((err) => displayError(err.message));
+  async function getGif() {
+    let apiKey = "xA2CejGIc8wKgASK0Ybc4brkr0pOS98A";
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=kobe`,
+      {
+        mode: "cors",
+      },
+    );
+    const gifData = await response.json();
+    displayGif(gifData);
+  }
+  getGif();
 }
 
 function displayGif(res) {
