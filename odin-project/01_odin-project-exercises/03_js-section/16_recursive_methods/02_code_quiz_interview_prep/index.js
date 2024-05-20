f272e0135b8917774ad03628037063e85845d108
CodeSwishKB <kylebarco04@gmail.com>
2024-05-21 07:53:33 +0800

    done quiz

--- odin-project/01_odin-project-exercises/03_js-section/16_recursive_methods/02_code_quiz_interview_prep/index.js
+++ odin-project/01_odin-project-exercises/03_js-section/16_recursive_methods/02_code_quiz_interview_prep/index.js
@@ -142,3 +142,46 @@ const totalIntegers = (arr) => {
 };
 var seven = totalIntegers([[[5], 3], 0, 2, ["foo"], [], [4, [5, 6]]]); // 7
 // console.log(seven)
+
+// Question 8:
+// Write a function that sums squares of numbers in list that may contain more lists
+const SumSquares = (arrNum) => {
+  // base case
+  if (arrNum.length === 0) return 0;
+
+  // variable total
+  let total = 0;
+
+  // loop the arrNum
+  for (let i = 0; i < arrNum.length; i++) {
+    // check if its array
+    if (Array.isArray(arrNum[i])) {
+      // add it to total recursively
+      total += SumSquares(arrNum[i]);
+    } else {
+      // after recursively check, if it is array square the items
+      total += arrNum[i] * arrNum[i];
+    }
+  }
+  return total;
+};
+var l = [1, 2, 3];
+// console.log(SumSquares(l)); // 1 + 4 + 9 = 14
+l = [[1, 2], 3];
+// console.log(SumSquares(l)); // 1 + 4 + 9 = 14
+l = [[[[[[[[[1]]]]]]]]];
+// console.log(SumSquares(l)); // 1 = 1
+l = [10, [[10], 10], [10]];
+// console.log(SumSquares(l)); // 100 + 100 + 100 + 100 = 400
+
+// Question 9:
+// The function should return an array containing repetitions of the number argument. For instance, replicate(3, 5) should return [5,5,5]. If the times argument is negative, return an empty array.
+const replicate = (times, n) => {
+  if (times <= 0) {
+    return [];
+  }
+  return [n].concat(replicate(times - 1, n));
+};
+console.log(replicate(3, 5)); // [5, 5, 5]
+console.log(replicate(1, 69)); // [69]
+console.log(replicate(-2, 6)); // []
